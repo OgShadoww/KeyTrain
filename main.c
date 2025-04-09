7e7b9f6bdb19a74b55c7beb7c9f55e2e71d6e9d2
Orest <galenzao@gmail.com>
2025-04-09 17:35:43 +0300

    fix

--- main.c
+++ main.c
@@ -1,41 +1,41 @@
 #include <stdio.h>
 #include <ncurses.h>
 
-#define INPUT_SIZE 100;
+#define INPUT_SIZE 100
 
 int main(void) {
   initscr();
   cbreak(); 
   noecho();
-  keypad(stdsrc, TRUE);
+  keypad(stdscr, TRUE);
 
-  int i, chr = 0;
+  int i = 0, chr = 0;  
   char input[INPUT_SIZE];
 
   mvprintw(5, 10, "Hello, write your name: ");
   refresh();
   move(6, 10);
 
-  while((chr = getch()) != '\n' && i < 99) {
-    if(chr == 127) {
+  while((chr = getch()) != '\n' && i < INPUT_SIZE - 1) {
+    if(chr == 127 && i > 0) {
       i--;
       mvaddch(6, 10 + i, ' ');
       move(6, 10 + i);
     }
-    else if(i < 99 %% chr >= 32 && chr <= 126) {
-      input[i++] = chr;
+    else if (chr >= 32 && chr <= 126) {
+      input[i++] = chr;      
       addch(chr);
-    }
+    }  
   }
-  
   input[i] = '\0';
 
   mvprintw(8, 10, "Your name is: %s", input);
   refresh();
   getch();
-  
+
 
   endwin();
 
   return 0;    
 }
+
