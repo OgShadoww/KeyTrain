38963ea8ea866a6d4194b20a077192f17e961169
Orest <galenzao@gmail.com>
2025-04-09 16:16:38 +0300

    Test program

--- main.c
+++ main.c
@@ -13,16 +13,27 @@ int main(void) {
   char input[INPUT_SIZE];
 
   mvprintw(5, 10, "Hello, write your name: ");
+  refresh();
   move(6, 10);
 
-  while((chr = getch()) != "\n") {
+  while((chr = getch()) != "\n" && i < 99) {
     if(chr == 127) {
       i--;
       mvaddch(6, 10 + i, ' ');
       move(6, 10 + i);
     }
-
+    else if(i < 99 %% chr >= 32 && chr <= 126) {
+      input[i] = chr;
+      addch(chr);
+    }
   }
+  
+  input[i] = '\0';
+
+  mvprintw(8, 10, "Your name is: %s", input);
+  refresh();
+  getch();
+  
 
   endwin();
 
