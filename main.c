d88368e19040c689d943d347031a49be0077fbe4
Orest <galenzao@gmail.com>
2025-04-09 15:36:23 +0300

    ncurses training program

--- main.c
+++ main.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 #include <ncurses.h>
 
+#define INPUT_SIZE 100;
+
 int main(void) {
   initscr();
-  
-  printw("Hello world");
-  refresh();
+  cbreak(); 
+  noecho();
+  keypad(stdsrc, TRUE);
 
-  getch();
+  int i, chr = 0;
+  char input[INPUT_SIZE];
+
+  mvprintw(5, 10, "Hello, write your name: ");
+  move(6, 10);
+
+  while((chr = getch()) != "\n") {
+    if(chr == 127) {
+      i--;
+      mvaddch(6, 10 + i, ' ');
+      move(6, 10 + i);
+    }
+
+  }
+
   endwin();
 
   return 0;    
